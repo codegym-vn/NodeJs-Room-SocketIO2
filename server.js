9bbe2b8e938f7057c7f8fe045261186c7e72e3e9
vukieuanh <vukieuanh.hnue@gmail.com>
2022-05-08 13:42:37 +0700

    add rooms

---
+++ server.js
@@ -0,0 +1,79 @@
+const { createServer } = require("http");
+const fs = require('fs');
+const { Server } = require("socket.io");
+const mimeTypes = {
+    "html": "text/html",
+    "js": "text/javascript",
+    "css": "text/css"
+};
+
+const httpServer = createServer(function (req, res){
+    if (req.url === '/') {
+        res.writeHead(200, { "Content-Type": "text/html" });
+        fs.createReadStream('./templates/index.html').pipe(res)
+    }
+    /* đọc file css/js*/
+    const filesDefences = req.url.match(/\.js|.css/);
+    if (filesDefences) {
+        const extension = mimeTypes[filesDefences[0].toString().split('.')[1]];
+        res.writeHead(200, { 'Content-Type': extension });
+        fs.createReadStream(__dirname + "/" + req.url).pipe(res)
+    }
+});
+
+const io = new Server(httpServer);
+
+const users = {}
+
+var usernames = {};
+
+var rooms = ['Lobby'];
+
+io.sockets.on('connection', function (socket) {
+
+    socket.on('adduser', function (username, nameRoom){
+        socket.username = username;
+        socket.room = nameRoom;
+        usernames[username] = username;
+        socket.join(nameRoom);
+        if (nameRoom != null && rooms.indexOf(nameRoom) <0){
+            rooms.push(nameRoom)
+        }
+        socket.emit('updatechat', 'SERVER', 'you have connected to '+ nameRoom);
+        socket.broadcast.to(nameRoom).emit('updatechat', 'SERVER', username + ' has connected to this room');
+        socket.emit('updaterooms', rooms, nameRoom);
+    });
+
+    socket.on('create', function (room) {
+        var room = 'test';
+        rooms.push(room);
+    });
+
+    socket.on('sendchat', function (data) {
+        io.sockets.in(socket.room).emit('updatechat', socket.username, data);
+    });
+
+    socket.on('switchRoom', function(newroom){
+        socket.leave(socket.room);
+        socket.join(newroom);
+        socket.emit('updatechat', 'SERVER', 'you have connected to '+ newroom);
+        socket.broadcast.to(socket.room).emit('updatechat', 'SERVER', socket.username+' has left this room');
+        socket.room = newroom;
+        socket.broadcast.to(newroom).emit('updatechat', 'SERVER', socket.username+' has joined this room');
+        socket.emit('updaterooms', rooms, newroom);
+    });
+
+
+
+    socket.on('disconnect', function(){
+
+        delete usernames[socket.username];
+        io.sockets.emit('updateusers', usernames);
+        socket.broadcast.emit('updatechat', 'SERVER', socket.username + ' has disconnected');
+        socket.leave(socket.room);
+    });
+});
+
+httpServer.listen(3000, 'localhost', function (){
+    console.log('Server running in http://localhost:3000')
+})
